cf55376bb6e4985bba8ca3e86f281c5e76f6297e
GeoreDimi <giorgosdimitropoulos7@gmail.com>
2018-06-25 14:37:50 +0200

    weiter User Funktions

--- Assets/Scripts/DB_Manager.cs
+++ Assets/Scripts/DB_Manager.cs
@@ -6,38 +6,39 @@ using System.Data;
 using Mono.Data.Sqlite;
 using UnityEngine.UI;
 
-public class DB_Manager : MonoBehaviour {
+public class DB_Manager : MonoBehaviour
+{
 
     //ConnectString
     private string connectString;
     private List<string> bestScores = new List<string>();
-    
+
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
 
         connectString = "URI=file:" + Application.dataPath + "/spaceDB.sqlite";
-       
+
 
 
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-    
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
     public void makePlayer(InputField userName)
     {
-        User.UserName = userName.text;
-            InsertUser(userName.text, 0, 0);
         
-        
+        InsertUser(userName.text, 0, 0);
     }
-    private void InsertUser(string name, int score = 0,int totalPoints = 0)
+    private void InsertUser(string name, int score = 0, int totalPoints = 0)
     {
         if (!UserExist(name))
         {
-            
+
             using (IDbConnection dbConnection = new SqliteConnection(connectString))
             {
                 dbConnection.Open();
@@ -59,7 +60,7 @@ public class DB_Manager : MonoBehaviour {
             dbConnection.Open();
             using (IDbCommand dbCmd = dbConnection.CreateCommand())
             {
-                string sqlQuery =String.Format("SELECT * FROM space_users WHERE name = \"{0}\"", name);
+                string sqlQuery = String.Format("SELECT * FROM space_users WHERE name = \"{0}\"", name);
                 dbCmd.CommandText = sqlQuery;
                 using (IDataReader reader = dbCmd.ExecuteReader())
                 {
@@ -67,20 +68,23 @@ public class DB_Manager : MonoBehaviour {
                     {
                         if (reader.GetString(1) != "")
                         {
+                            User.SetUserData(reader.GetString(0), reader.GetString(1), reader.GetString(2));
                             return true;
                         }
                     }
 
                     reader.Close();
                     dbConnection.Close();
-                    
+
                 }
             }
 
+            User.SetUserData(name);
             return false;
         }
     }
 
+   
     public void FillListView(Text Listtxt)
     {
         GetScores();
@@ -96,7 +100,7 @@ public class DB_Manager : MonoBehaviour {
         using (IDbConnection dbConnection = new SqliteConnection(connectString))
         {
             dbConnection.Open();
-            
+
             using (IDbCommand dbCmd = dbConnection.CreateCommand())
             {
                 string sqlQuery = "SELECT * FROM space_users ORDER BY best_score DESC";
@@ -104,7 +108,8 @@ public class DB_Manager : MonoBehaviour {
                 using (IDataReader reader = dbCmd.ExecuteReader())
                 {
                     while (reader.Read())
-                    { counter++;
+                    {
+                        counter++;
                         bestScores.Add(counter.ToString() + "  " + reader.GetString(0) + "  " + reader.GetString(1));
                         //Debug.Log(reader.GetString(0) + " " + reader.GetString(1));
                     }
--- Assets/Scripts/User.cs
+++ Assets/Scripts/User.cs
@@ -1,7 +1,8 @@
 ﻿public static class User
 {
     private static string userName;
-   
+    private static string bestScore;
+    private static string totalPoints;
 
     public static string UserName
     {
@@ -14,5 +15,52 @@
         {
             userName = value;
         }
+    }
+
+    public static string BestScore
+    {
+        get
+        {
+            return bestScore;
+        }
+
+        set
+        {
+            bestScore = value;
+        }
+    }
+
+    public static string TotalPoints
+    {
+        get
+        {
+            return totalPoints;
+        }
+
+        set
+        {
+            totalPoints = value;
+        }
+    }
+
+
+    public static void SetUserData(string username, string bestScore = "0", string totalPoints = "0")
+    {
+        UserName = username;
+        BestScore = bestScore;
+        TotalPoints = totalPoints;
+    }
+
+    public static string GetUserDataMessage(bool newPlayer)
+    {
+        if (newPlayer)
+        {
+            return string.Format("Welcome to Space 101 {0} ", UserName, BestScore, TotalPoints);
+        }
+        else
+        {
+            return string.Format("Welcome back {0} \n your Best Score is {1} and your Total Points are {2}", UserName, BestScore, TotalPoints);
+        }
+        
     }
 }
