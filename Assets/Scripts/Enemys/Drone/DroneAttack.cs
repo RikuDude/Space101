068df7bdd1e09479e53ec29e4ea258b5005cc0c7
thorsten <thorsten@h1z.de>
2018-06-25 14:34:52 +0200

    fixed UI and added a new enemy

---
+++ Assets/Scripts/Enemys/Drone/DroneAttack.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DroneAttack : MonoBehaviour {
+
+    private GameObject targetPlayer;
+
+    private float timeUntilAquieringNextTarget = 5;
+
+    public float fireRate = 5f;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    private void aquireTargetPlayer()
+    {
+
+        GameObject[] allPlayers = GameObject.FindGameObjectsWithTag("Player");
+        if (allPlayers.Length > 0) {
+
+            GameObject closestPlayer = allPlayers[0];
+
+            foreach (GameObject player in allPlayers)
+            {
+                if(Vector2.Distance(this.transform.position, player.transform.position) < Vector2.Distance(this.transform.position, closestPlayer.transform.position))
+                {
+                    closestPlayer = player;
+                }                
+            }
+
+            targetPlayer = closestPlayer;
+        }
+    }
+
+    private void ShootAtTargetPlayer()
+    {
+
+    }
+
+}
---
+++ Assets/Scripts/Enemys/Drone/DroneMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DroneMovement : MonoBehaviour {
+
+    public float droneSpeed = 10f;
+
+    public Transform targetBoundaryHolder;
+
+    private Vector2 movementDirection;
+
+    // Use this for initialization
+    void Start () {
+        targetBoundaryHolder = GameObject.Find("TargetBoundary").transform;
+
+        Vector2 targetPosition = new Vector2(Random.Range(targetBoundaryHolder.GetChild(1).position.y, targetBoundaryHolder.GetChild(0).position.y), Random.Range(targetBoundaryHolder.GetChild(2).position.x, targetBoundaryHolder.GetChild(3).position.x));
+
+        movementDirection = targetPosition - new Vector2(this.transform.position.x, this.transform.position.y);
+
+        movementDirection.Normalize();
+
+        turnToMovementDirection(movementDirection);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+        transform.Translate(movementDirection.x * Time.deltaTime * droneSpeed, movementDirection.y * Time.deltaTime * droneSpeed, 0, Space.World);
+        
+    }
+
+
+
+    private void turnToMovementDirection(Vector2 movementDirection)
+    {
+
+        float angle = Mathf.Atan2(movementDirection.y, movementDirection.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, angle + 270f);
+
+    }
+
+}
