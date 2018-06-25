3e6a93106d753c166356919aaafe39ce34f9b3bc
GeoreDimi <giorgosdimitropoulos7@gmail.com>
2018-06-25 14:03:10 +0200

    Username in Game Scene

---
+++ Assets/Scripts/Set_User.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Set_User : MonoBehaviour {
+    
+
+   
+
+   
+
+    //public Text nameBox;
+    public void OnEnable()
+    {
+       
+        Text nameBox = GameObject.Find("Text_Box").GetComponent<Text>(); //as Text;
+        nameBox.text = User.UserName;
+        User.UserName = "";
+      
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/User.cs
@@ -0,0 +1,18 @@
+﻿public static class User
+{
+    private static string userName;
+   
+
+    public static string UserName
+    {
+        get
+        {
+            return userName;
+        }
+
+        set
+        {
+            userName = value;
+        }
+    }
+}
