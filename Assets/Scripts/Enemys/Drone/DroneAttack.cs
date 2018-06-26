50462eab9146ee8a09368dd8acbbe6c4dbc80b46
thorsten <thorsten@h1z.de>
2018-06-26 16:39:53 +0200

    implemented enemy drone shooting mechanic

--- Assets/Scripts/Enemys/Drone/DroneAttack.cs
+++ Assets/Scripts/Enemys/Drone/DroneAttack.cs
@@ -6,17 +6,38 @@ public class DroneAttack : MonoBehaviour {
 
     private GameObject targetPlayer;
 
-    private float timeUntilAquieringNextTarget = 5;
+    public GameObject droneProjectile;
+
+    public Transform playerBoundary;
+
+
+
+    private float timeUntilAquieringNextTarget = 5f;
+    private float nextTimeToAquireNextTarget = 0;
 
     public float fireRate = 5f;
+    private float nextTimeToFire = 0;
 
     void Start () {
-		
-	}
+        playerBoundary = GameObject.Find("PlayerBoundary").transform;
+    }
 
 	void Update () {
-		
-	}
+
+        if (Time.time >= nextTimeToAquireNextTarget)
+        {
+            aquireTargetPlayer();
+            nextTimeToAquireNextTarget = Time.time + timeUntilAquieringNextTarget;
+        }
+
+        if (Time.time >= nextTimeToFire && isOnScreen())
+        {
+            nextTimeToFire = Time.time + 1f / fireRate;
+            ShootAtTargetPlayer();
+
+            //Debug.Log("Fires at target player!");
+        }
+    }
 
 
     private void aquireTargetPlayer()
@@ -41,7 +62,28 @@ public class DroneAttack : MonoBehaviour {
 
     private void ShootAtTargetPlayer()
     {
+        Instantiate(droneProjectile, transform.position, rotateProjectileTowardsTarget(targetPlayer.transform.position));
+    }
 
+    private Quaternion rotateProjectileTowardsTarget(Vector2 targetPosition)
+    {
+        
+        Vector2 normalizedShootingDirection = targetPosition - new Vector2(this.transform.position.x, this.transform.position.y);
+
+        normalizedShootingDirection.Normalize();
+        
+        float angle = Mathf.Atan2(normalizedShootingDirection.y, normalizedShootingDirection.x) * Mathf.Rad2Deg;
+        return Quaternion.Euler(0, 0, angle + 270f);
+        
+    }
+
+    private bool isOnScreen()
+    {
+        if (this.transform.position.y < playerBoundary.GetChild(0).transform.position.y && this.transform.position.y > playerBoundary.GetChild(1).transform.position.y && this.transform.position.x > playerBoundary.GetChild(2).transform.position.x && this.transform.position.y < playerBoundary.GetChild(3).transform.position.x)
+        {
+            return true;
+        }
+        return false;
     }
 
 }
--- Assets/Scripts/Enemys/Drone/DroneProjectile.cs
+++ Assets/Scripts/Enemys/Drone/DroneProjectile.cs
@@ -6,6 +6,18 @@ public class DroneProjectile : MonoBehaviour {
 
     public float damage = 5f;
 
+    public float speed;
+    Rigidbody2D myRigidbody;
+    
+        void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+    }
+    
+    void Update()
+    {
+        myRigidbody.velocity = transform.TransformDirection(new Vector2(0f, 1f) * speed);
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -24,4 +36,7 @@ public class DroneProjectile : MonoBehaviour {
         }
         
     }
+
+    
+
 }
